7a48959d4de1acd7ce10105ad3d29636f6785097
Tyler <MizellT@github.com>
2022-12-10 19:56:07 -0700

    completed simple addition calulator

---
+++ Apples/src/oranges/bananas.java
@@ -0,0 +1,10 @@
+
+package oranges;
+import java.util.Scanner;
+
+public class bananas {
+	public static void main (String args[]) {
+		Scanner bucky = new Scanner (System.in);
+		System.out.println(bucky.nextLine());
+	}
+}
